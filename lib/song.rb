b1a7eb8195496a2a2dcd8b6cc57bf97871d5893f
Sara Amiry <saraamiry24@gmail.com>
2020-06-23 00:57:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,11 @@ class Song
   
   def self.artist_count 
     artist_count = {}
-    @@
+    @@artists.each do |artist|
+      if !artist_count[artist]
+        artist_count = 1 
+      else 
+        
   end
   
   def self.genre_count
